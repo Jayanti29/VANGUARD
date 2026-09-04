c9a79aee1b3d963b2198ad4b6f67f0aa2f6920ba
Jayanti Gautam <jayanti102024@gmail.com>
2026-09-04 18:24:56 +0530

    feat(data): add national and state agriculture subsidy datasets

---
+++ client/src/data/agriSubsidies.js
@@ -0,0 +1,1 @@
+export const SUBSIDIES = [{ id: "pm-kusum", name: "PM-KUSUM", benefit: "60% Solar Pump Subsidy" }, { id: "pdmc", name: "Per Drop More Crop", benefit: "55% Drip Irrigation Subsidy" }];
