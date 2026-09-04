dfa23b18d3b8a380cb235f970dc7f250fc41d1bd
Jayanti Gautam <jayanti102024@gmail.com>
2026-09-04 18:24:57 +0530

    feat(utils): add regional speech recognition language code resolver

---
+++ client/src/utils/speechUtils.js
@@ -0,0 +1,1 @@
+export const getSpeechLang = (code) => ({ en: "en-IN", hi: "hi-IN", kn: "kn-IN", ta: "ta-IN", te: "te-IN", ml: "ml-IN", bn: "bn-IN", mr: "mr-IN", gu: "gu-IN", pa: "pa-IN" }[code] || "en-IN");
